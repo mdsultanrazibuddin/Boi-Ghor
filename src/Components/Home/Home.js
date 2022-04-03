3bbaba934789781cf647b38a4994571d09ef0e0c
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-04-03 22:09:27 +0600

    article added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,19 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            
+           <div>
+                <div>
+                    <h1> Recharge Your Down Battery</h1>
+                    <h2>By Jhankar Mahbub</h2>
+                    <p>The book Recharge Your Down Battery basically teaches us how to deal with various problems or stress in our daily life. Throughout the book, the author Jhangkar Mahbub has beautifully highlighted the various problems of the young generation through the dialogue-based conversations of Masum Bhai and Abir respectively...</p>
+                    <button>Read More</button>
+                </div>
+                <div>
+                    
+                </div>
+           </div>
+
+
         </div>
     );
 };
