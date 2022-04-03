71b57ee70a0ba1ac2795a06647e8b16050e654b0
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-04-03 21:05:59 +0600

    icon+title added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+// import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>amr nam sultan razib uddin</h1>
+
+     {/* <Routes>
+     <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+     </Routes> */}
+    </div>
+  );
+}
+
+export default App;
