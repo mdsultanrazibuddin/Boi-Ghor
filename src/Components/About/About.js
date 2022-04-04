eec0b0f529399541e2e9652d04e6ec8ac90a0972
mdsultanrazibuddin <mdsultanrazibuddin@gmail.com>
2022-04-05 02:53:20 +0600

    about added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
         <div>
-            
+            <h1 className='text'>About Of Boi-Ghor Website</h1>
+            <div className='context'>
+                <h1>Boi-Ghor Website</h1>
+                <p > 1. My website was originally created for book reviews. <br />
+
+                   2. My website consists of customer reviews, dashboards, blogs. <br />
+                   3. React's icon and Recharts has been used on my website. <br />
+                   4. Some components, custom hooks, distructuring have been used to enhance the beauty of my website code.</p>
+            </div>
         </div>
     );
 };
